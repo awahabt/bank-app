3ac6dfba994444f6309f1159079a461a32c6e2ba
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-04-26 22:44:57 +0500

    add

---
+++ react-bank-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './style'
+
+import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components';
+  
+  const App = () => (
+    <div className="w-full h-screen bg-zinc-800 ">
+      < Navbar />
+    </div>
+  );
+
+export default App
